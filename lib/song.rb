dbb2f9e28dbed21853d51cecce6ff7e49f5f7240
Kayla Kingston <kkingston09@gmail.com>
2020-06-08 20:26:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,14 @@
 class Song 
   
   @@count = 0 
+  @@artist = []
   
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
     @@count += 1
+    @@artist << @artist
   end
   
   def name
